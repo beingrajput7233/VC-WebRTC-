800472a1748f9973448a1625626be0946dea4806
Ashutosh <rajputashu7200@gmail.com>
2023-08-26 12:36:55 +0530

    Lobby frontend

---
+++ client/my/src/screens/Lobby.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState ,useCallback} from "react";
+// lobby mein user id and room number lenge
+const LobbyScreen=()=>{
+    const [email,setEmail]=useState("");
+    const [room,setRoom]=useState("");
+    {/*  form submission */}
+
+    const handleSubmitForm=useCallback((e)=>{
+        e.preventDefault();
+        console.log({
+            email,
+            room,
+        })
+    },[])
+
+    return (
+        <div>
+            <h1>Lobby</h1>
+            <form onSubmit={handleSubmitForm}>
+                <label htmlFor="email">Email Id</label>
+                <input type="email" id="email" value={email} onChange={e=>setEmail(e.target.value)} />
+                <br/>
+                <label htmlFor="room">Room Number</label>
+                <input type="text" id="room" value={room} onChange={e=>setRoom(e.target.value)}/>
+                <br />
+                <button type="submit">Join</button>
+            </form>
+        </div>
+    )
+}
+
+export default LobbyScreen;
